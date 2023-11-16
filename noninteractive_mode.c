e845e097968dd2ed259ca25734e81b8ee2cfdd45
Kennjenga <108663105+Kennjenga@users.noreply.github.com>
2023-11-16 15:10:31 +0300

    Update noninteractive_mode.c

--- noninteractive_mode.c
+++ noninteractive_mode.c
@@ -16,19 +16,19 @@ void non_interactive(void)
 	{
 		while (getline(&line, &n, stdin) != -1)
 		{
-			remove_newline(line);
-			remove_comment(line);
-			commands = tokenizer(line, ";");
+			del_line(line);
+			toggle(line);
+			commands = token(line, ";");
 			for (i = 0; commands[i] != NULL; i++)
 			{
-				command = tokenizer(commands[i], " ");
+				command = token(commands[i], " ");
 				if (command[0] == NULL)
 				{
 					free(command);
 					break;
 				}
 				cmdtype = parse_command(command[0]);
-				innitr(command, cmdtype);
+				innit(command, cmdtype);
 				free(command);
 			}
 			free(commands);
